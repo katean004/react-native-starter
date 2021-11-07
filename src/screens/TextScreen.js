fbe455a2efc8cfddd5a5bc8098a23a48a1068ab1
kate <kan004@ucr.edu>
2021-11-06 17:44:34 -0700

    added state to textinput

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, Button, View, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <View>
+      <Text>Enter Name:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={name}
+        onChangeText={newValue => {
+          setName(newValue);
+        }}
       />
+      <Text>My name is {name}</Text>
+
+      <Text>Enter Password:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={password}
+        onChangeText={newPass => setPassword(newPass)}
+        secureTextEntry={true}
+      />
+      <Text>My password is {password}</Text>
+      {password.length < 5 ? (
+        <Text>Password has to be at least 5 chars</Text>
+      ) : null}
     </View>
   );
 };
